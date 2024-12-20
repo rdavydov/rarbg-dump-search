89c950aab3e6406aeb5003026581c7e099276427
Roman Davydov <15850461+rdavydov@users.noreply.github.com>
2024-12-21 01:05:36 +0300

    Update backend-2dbs.py

--- backend-2dbs.py
+++ backend-2dbs.py
@@ -25,7 +25,7 @@ app.add_middleware(
     allow_origins=[
         "https://rdavydov.github.io",
         "https://rarbg-ten.vercel.app",
-    # ],
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
